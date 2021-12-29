454e3cf6dbe1cec52f5031be30c621defa60f782
SurajTC <surzavon@gmail.com>
2021-12-29 20:31:22 +0530

    :heavy_plus_sign: Host URL

--- admin-frontend-next/utils/getHostURL.js
+++ admin-frontend-next/utils/getHostURL.js
@@ -2,6 +2,6 @@ export function hostURL() {
   if (process.env.NODE_ENV == "development") {
     return "http://localhost:3000/"
   } else {
-    return "https://surz-university.netlify.app/"
+    return "https://university-administration.vercel.app/"
   }
 }
